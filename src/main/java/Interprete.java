314cc8d8b17a619cc22469805a9400224f1c58e4
gua21150 <gua21150@uvg.edu.gt>
2022-03-08 11:03:16 -0600

    stacks

--- src/main/java/Interprete.java
+++ src/main/java/Interprete.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Interprete {
 
@@ -8,8 +9,19 @@ public class Interprete {
         ArrayList<Character> characters = new ArrayList<>(); // lista temporal que contiene los caracteres
         file.readFile(list);                                 // lectura del archivo
         characters.addAll(file.getCharactersList(list));     // se convierte la lista a una lista de caracteres
+
+        Stack stack1 = new Stack();
+        int contador=0;
+        list.clear();
         for (Character chars: characters) {
             System.out.println(chars);                       // solo para ver la lectura realizada
+
+            if(chars =='(') {
+                int fin = characters.size();
+                for (int i = contador+1; i < fin; i++) {
+                    list.add(characters.get(i)+"");
+                }
+            }
         }
     }
 }

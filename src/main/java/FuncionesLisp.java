7c3fe800743747cd946e9da744abf757b6bd6937
gua21150 <gua21150@uvg.edu.gt>
2022-03-15 20:42:27 -0600

    Modificaciones

--- src/main/java/FuncionesLisp.java
+++ src/main/java/FuncionesLisp.java
@@ -3,23 +3,28 @@ import java.util.HashMap;
 
 public class FuncionesLisp {
     private ArrayList<Nodo> secuencia = new ArrayList<>();
+    /*
     private ArrayList<String> sucio = new ArrayList<>();
     public void operar(){
         // oh no
         this.sucio = Read.readFile("lispExpression.txt",sucio);
 
-    }
+    }*/
 
-    public ArrayList<Nodo> convert(ArrayList<String> expression) {
+    public ArrayList<Nodo> convert(ArrayList<Nodo> expression) {
         ArrayList<Nodo> respuesta = new ArrayList<Nodo>();
-        String temp = "";
+        Nodo nodo; 
+               
         for (int i = 1; i < expression.size(); i++) {
             if(expression.get(i).equals("(")) {
-                respuesta.add(convert(expression));
+                respuesta.remove(0);
+                Nodo temp = new Nodo(convert(expression));
+                respuesta.add(temp);
             } else {
                 if(expression.get(i).equals(")")){
                     return respuesta;
                 } else{
+                    
                     respuesta.add(expression.get(i));
                     expression.remove(i);
                 }
@@ -28,12 +33,12 @@ public class FuncionesLisp {
     }
 
     /*
-    public ArrayList<Nodo> convert(ArrayList<String> expression) {
+    public ArrayList<Nodo> convert(ArrayList<Nodo> expression) {
         ArrayList<Integer> posiciones = new ArrayList<>(); // sirve para colocar las posiciones de cada parentesis de apertura
         int posicion = 0;
         int parentesisAbiertos=0;
 
-        for (String str : expression) {
+        for (Nodo str : expression) {
             if(str.equals("(")) {                
                 parentesisAbiertos++;            
         }
@@ -41,7 +46,7 @@ public class FuncionesLisp {
         for (int i = 0; i < array.length; i++) {
             
         }
-        for (String string : expression) {
+        for (Nodo Nodo : expression) {
             if(str.equals("(")) {
                 posicion++;
                 parentesisAbiertos++;
